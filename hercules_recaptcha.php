4804c6825233f9545ed94d6b30241685b112b5fc
jdavis916 <jasondavis3@gmail.com>
2015-01-20 22:03:56 -0800

    formatting cleaned up

--- hercules_recaptcha.php
+++ hercules_recaptcha.php
@@ -1,28 +1,26 @@
 <?php
-/*
-Plugin Name: Herc Recaptcha
-Description: Adds a recaptcha with the new Google Recaptcha API to the comment form for anonymous users
-Author: Todd D. Nestor - todd.nestor@gmail.com
-Version: 1.0
-License: GNU General Public License v3 or later
-License URI: http://www.gnu.org/licenses/gpl-3.0.html
-*/
+    /*
+    Plugin Name: Herc Recaptcha
+    Description: Adds a recaptcha with the new Google Recaptcha API to the comment form for anonymous users
+    Author: Todd D. Nestor - todd.nestor@gmail.com
+    Version: 1.0
+    License: GNU General Public License v3 or later
+    License URI: http://www.gnu.org/licenses/gpl-3.0.html
+    */
 
-/**
- * Class HercRecaptcha adds Recaptcha to the specified forms.
- *
- * The new Google Recaptcha API still uses javascript, but the check will fail if the item isn't submitted,
- * that is the form is useless with javascript disabled.
- */
+    /**
+     * Class HercRecaptcha adds Recaptcha to the specified forms.
+     *
+     * The new Google Recaptcha API still uses javascript, but the check will fail if the item isn't submitted,
+     * that is the form is useless with javascript disabled.
+     */
     class HercRecaptcha
     {
-
         /**
          * This constructor loads the scripts for Google's Recaptcha and adds the Recaptcha form to the comments form.
          */
         function __construct()
         {
-
             $this->SetRecaptchaKeys();
             $current_theme = get_current_theme();
             
@@ -48,7 +46,7 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
         
         function TestRegistrationForm()
         {
-            echo 'i am right here!!!!!!!!!!!!';
+            echo 'I am right here!!!!!!!!!!!!';
         }
         
         /**
@@ -87,143 +85,141 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
             ?>
                 <div class="wrap">
                     <h2>Recaptcha Settings</h2>
-                        <table class="form-table">
-                            <?php if( !empty( $success_msg ) ): ?>
-                            <tr valign="top">
-                                <th colspan="2" scope="row">
-                                    <span style="color:red; font-weight:bold;"><?php echo $success_msg; ?></span>
-                                </th>
-                            </tr>
-                            <?php endif; ?>
-                            <tr valign="top">
-                                <th colspan="2" scope="row">
-                                    If you don't have these keys yet go to <a href="https://www.google.com/recaptcha/" target="_blank">https://www.google.com/recaptcha/</a> to obtain keys.
-                                    <br><br>
-                                    If the Recaptcha isn't showing up try a different placement.  Also, some other comment plugins (such as Jetpack comments) interfere with this plugin and they will not work together.
-                                </th>
-                            </tr>
-                            <tr valign="top">
-                                <th scope="row">
-                                    <form method="POST" action="">
-                                    <input type="hidden" name="update_herc_recaptcha_settings" value="Y" />
-                                    <label for="herc_recaptcha_options[public_key]">
-                                        Site Key
-                                    </label> 
-                                </th>
-                                <td>
-                                    <input type="text" name="herc_recaptcha_options[public_key]" size="25" value="<?php echo $this->public_recaptcha_key;?>" />
-                                </td>
-                            </tr>
-                            <tr valign="top">
-                                <th scope="row">
-                                    <label for="herc_recaptcha_options[private_key]">
-                                        Secret Key
-                                    </label> 
-                                </th>
-                                <td>
-                                    <input type="text" name="herc_recaptcha_options[private_key]" size="25" value="<?php echo $this->private_recaptcha_key;?>" />
-                                </td>
-                            </tr>
-                            <tr valign="top">
-                                <th colspan="2" scope="row">
-                                    <h3>
-                                        Comment Form
-                                    </h3> 
-                                </th>
-                            </tr>
-                            <tr valign="top">
-                                <th scope="row">
-                                    <label for="herc_recaptcha_options[registration_form]">
-                                        Show on Comment Form
-                                    </label>
-                                </th>
-                                <td>
-                                    <input type="hidden" name="herc_recaptcha_options[comment_form]" value="false" />
-                                    <input type="checkbox" name="herc_recaptcha_options[comment_form]" value="true" <?php echo $this->comment_form == 'false' ? '' : 'checked="checked"'; ?> />
-                                </td>
-                            </tr>
-                            <tr valign="top">
-                                <th scope="row">
-                                    <label for="herc_recaptcha_options[placement]">
-                                        Location: 
-                                    </label> 
-                                </th>
-                                <td>
-                                    <select name="herc_recaptcha_options[placement]">
-                                        <?php
-                                        foreach( $placement_options as $key=>$val )
-                                        {
-                                        ?>
+                    <table class="form-table">
+                        <?php if( !empty( $success_msg ) ): ?>
+                        <tr valign="top">
+                            <th colspan="2" scope="row">
+                                <span style="color:red; font-weight:bold;"><?php echo $success_msg; ?></span>
+                            </th>
+                        </tr>
+                        <?php endif; ?>
+                        <tr valign="top">
+                            <th colspan="2" scope="row">
+                                If you don't have these keys yet go to <a href="https://www.google.com/recaptcha/" target="_blank">https://www.google.com/recaptcha/</a> to obtain keys.
+                                <br/><br/>
+                                If the Recaptcha isn't showing up try a different placement.  Also, some other comment plugins (such as Jetpack comments) interfere with this plugin and they will not work together.
+                            </th>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <form method="POST" action="">
+                                <input type="hidden" name="update_herc_recaptcha_settings" value="Y" />
+                                <label for="herc_recaptcha_options[public_key]">
+                                    Site Key
+                                </label> 
+                            </th>
+                            <td>
+                                <input type="text" name="herc_recaptcha_options[public_key]" size="25" value="<?php echo $this->public_recaptcha_key;?>" />
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <label for="herc_recaptcha_options[private_key]">
+                                    Secret Key
+                                </label> 
+                            </th>
+                            <td>
+                                <input type="text" name="herc_recaptcha_options[private_key]" size="25" value="<?php echo $this->private_recaptcha_key;?>" />
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th colspan="2" scope="row">
+                                <h3>
+                                    Comment Form
+                                </h3> 
+                            </th>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <label for="herc_recaptcha_options[registration_form]">
+                                    Show on Comment Form
+                                </label>
+                            </th>
+                            <td>
+                                <input type="hidden" name="herc_recaptcha_options[comment_form]" value="false" />
+                                <input type="checkbox" name="herc_recaptcha_options[comment_form]" value="true" <?php echo $this->comment_form == 'false' ? '' : 'checked="checked"'; ?> />
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <label for="herc_recaptcha_options[placement]">
+                                    Location: 
+                                </label> 
+                            </th>
+                            <td>
+                                <select name="herc_recaptcha_options[placement]">
+                                    <?php
+                                    foreach( $placement_options as $key=>$val )
+                                    {
+                                    ?>
                                         <option value="<?php echo $key; ?>" <?php echo $key == $this->placement ? "selected" : ""; ?>><?php echo $val; ?></option>
-                                        <?php
-                                        }
-                                        ?>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr valign="top">
-                                <th colspan="2" scope="row">
-                                    <h3>
-                                        Registration Form
-                                    </h3> 
-                                </th>
-                            </tr>
-                            <tr valign="top">
-                                <th scope="row">
-                                    <label for="herc_recaptcha_options[registration_form]">
-                                        Show on Registration Form
-                                    </label>
-                                </th>
-                                <td>
-                                    <input type="hidden" name="herc_recaptcha_options[registration_form]" value="false" />
-                                    <input type="checkbox" name="herc_recaptcha_options[registration_form]" value="true" <?php echo $this->registration_form == 'false' ? '' : 'checked="checked"'; ?> />
-                                </td>
-                            </tr>
-                            <tr valign="top">
-                                <th scope="row">
-                                    <label for="herc_recaptcha_options[placement]">
-                                        Style: 
-                                    </label> 
-                                </th>
-                                <td>
-                                    <select name="herc_recaptcha_options[style]">
-                                        <?php
-                                        $styles = array(
-                                            'dark'      => 'Dark',
-                                            'light'     => 'Light',
-                                        );
-                                        foreach( $styles as $key=>$val )
-                                        {
-                                        ?>
-                                        <option value="<?php echo $key; ?>" <?php echo $key == $this->style ? "selected" : ""; ?>><?php echo $val; ?></option>
-                                        <?php
-                                        }
-                                        ?>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    
-                                </th>
-                                <td>
-                                    <input type="submit" />
-                                    </form>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    
-                                </th>
-                                <td>
-                                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                                        <input type="hidden" name="cmd" value="_s-xclick">
-                                        <input type="hidden" name="hosted_button_id" value="GDBHPL4Y24ZXQ">
-                                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                                    </form>
-                                </td>
-                            </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th colspan="2" scope="row">
+                                <h3>
+                                    Registration Form
+                                </h3> 
+                            </th>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <label for="herc_recaptcha_options[registration_form]">
+                                    Show on Registration Form
+                                </label>
+                            </th>
+                            <td>
+                                <input type="hidden" name="herc_recaptcha_options[registration_form]" value="false" />
+                                <input type="checkbox" name="herc_recaptcha_options[registration_form]" value="true" <?php echo $this->registration_form == 'false' ? '' : 'checked="checked"'; ?> />
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <th scope="row">
+                                <label for="herc_recaptcha_options[placement]">
+                                    Style: 
+                                </label> 
+                            </th>
+                            <td>
+                                <select name="herc_recaptcha_options[style]">
+                                    <?php
+                                    $styles = array(
+                                        'dark'      => 'Dark',
+                                        'light'     => 'Light',
+                                    );
+                                    foreach( $styles as $key=>$val )
+                                    {
+                                    ?>
+                                    <option value="<?php echo $key; ?>" <?php echo $key == $this->style ? "selected" : ""; ?>><?php echo $val; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">                                   
+                            </th>
+                            <td>
+                                <input type="submit" />
+                                </form>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">                                    
+                            </th>
+                            <td>
+                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                    <input type="hidden" name="cmd" value="_s-xclick">
+                                    <input type="hidden" name="hosted_button_id" value="GDBHPL4Y24ZXQ">
+                                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                                </form>
+                            </td>
+                        </tr>
                     </table>
                 </div>
             <?php
